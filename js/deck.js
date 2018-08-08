6f1f00374be9bc1b0222cc41e7f1191888f5184a
Marissa Schmucker <marissashcmucker@gmail.com>
2018-08-08 13:33:38 -0500

    first commit

---
+++ js/deck.js
@@ -0,0 +1,10 @@
+const cardDeck = {
+    "ace":"img/deck/ace.png",
+    "queen":"img/deck/queen.png",
+    "king":"img/deck/king.png",
+    "jack":"img/deck/jack.png",
+    "hearts":"img/deck/hearts.png",
+    "diamonds":"img/deck/diamonds.png",
+    "clubs":"img/deck/clubs.png",
+    "spades":"img/deck/spades.png"
+};
